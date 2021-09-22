de9babde62d730c56657c5663a8ddb9d6b1b062c
mrtripie <65431647+mrtripie@users.noreply.github.com>
2021-09-22 10:47:38 -0400

    Simplify opacity and optimize
    Simplified the opacity by just multiplying it into the cel's 
    
    Optimized by skipping fully transparent cels.
    
    Optimized by using file.seek() to skip over the cel data when that cel isn't visible (before it created the image for that cel and just skipped blending it in).

--- addons/godot_pixelorama_importer/util/read_pxo_file.gd
+++ addons/godot_pixelorama_importer/util/read_pxo_file.gd
@@ -36,6 +36,8 @@ static func read_pxo_file(source_file: String, image_save_path: String):
 	var spritesheet = Image.new()
 	spritesheet.create(size.x * frame_count, size.y, false, Image.FORMAT_RGBA8)
 
+	var cel_data_size: int = size.x * size.y * 4
+
 	for i in range(frame_count):
 		var frame = project.frames[i]
 
@@ -43,45 +45,34 @@ static func read_pxo_file(source_file: String, image_save_path: String):
 		var frame_img: Image = null
 		var layer := 0
 		for cel in frame.cels:
-			# Load the cel image
-			var cel_img := Image.new()
-			cel_img.create_from_data(size.x, size.y, false, Image.FORMAT_RGBA8, file.get_buffer(size.x * size.y * 4))
-			
-			if project.layers[layer].visible:
-				var opacity: float = cel.opacity
-				if opacity == 1.0: # Avoid extra gd script calulations:
-					if frame_img == null:
-						frame_img = cel_img
-					else:
-						# Overlay each Cel on top of each other
-						frame_img.blend_rect(cel_img, Rect2(Vector2.ZERO, size), Vector2.ZERO)
-				else: # Blend with cel opacity:
-					if frame_img == null:
-						frame_img = cel_img
-						frame_img.lock()
-						for x in range(size.x):
-							for y in range(size.y):
-								var color := frame_img.get_pixel(x, y)
-								color.a *= opacity
-								frame_img.set_pixel(x, y, color)
-						frame_img.unlock()
-					else:
-						# Overlay each Cel on top of each other with opacity
-						frame_img.lock()
-						cel_img.lock()
-						for x in range(size.x):
-							for y in range(size.y):
-								var frame_col := frame_img.get_pixel(x, y)
-								var cel_col := cel_img.get_pixel(x, y)
-								var a := cel_col.a
-								cel_col.a = 1
-								var color := frame_col.linear_interpolate(cel_col, opacity * a)
-								frame_img.set_pixel(x, y, color)
-						frame_img.unlock()
-						cel_img.unlock()
+			var opacity: float = cel.opacity
+
+			if project.layers[layer].visible and opacity > 0.0:
+				# Load the cel image
+				var cel_img := Image.new()
+				cel_img.create_from_data(size.x, size.y, false, Image.FORMAT_RGBA8, file.get_buffer(cel_data_size))
+
+				if opacity < 1.0:
+					cel_img.lock()
+					for x in range(size.x):
+						for y in range(size.y):
+							var color := cel_img.get_pixel(x, y)
+							color.a *= opacity
+							cel_img.set_pixel(x, y, color)
+					cel_img.unlock()
+
+				if frame_img == null:
+					frame_img = cel_img
+				else:
+					# Overlay each Cel on top of each other
+					frame_img.blend_rect(cel_img, Rect2(Vector2.ZERO, size), Vector2.ZERO)
+			else:
+				# Skip this cel's data
+				file.seek(file.get_position() + cel_data_size)
+
 			layer += 1
 
-		if frame_img:
+		if frame_img != null:
 			# Add to the spritesheet
 			spritesheet.blit_rect(frame_img, Rect2(Vector2.ZERO, size), Vector2((size.x * i), 0))
 

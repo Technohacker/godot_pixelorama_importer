7dd660d81f8c98f4ff08871b5f9b5377fd266cba
mrtripie <65431647+mrtripie@users.noreply.github.com>
2021-09-22 11:15:02 -0400

    Added support for layer visibility and cel opacity (#4)
    * Added support for layer visibility and cel opacity
    
    Turning off the visibility of layers in Pixelorama will now result in them being skipped in the plugin.
    
    Adjusting the cel's opacity in Pixelorama will also result in the cels being blended into the frame with a different opacity. Right now it has the old blending code if the opacity is equal to 1 as the opacity code should be slower.
    
    I can create screenshots if needed.
    
    * Simplify opacity and optimize
    
    Simplified the opacity by just multiplying it into the cel's 
    
    Optimized by skipping fully transparent cels.
    
    Optimized by using file.seek() to skip over the cel data when that cel isn't visible (before it created the image for that cel and just skipped blending it in).

--- addons/godot_pixelorama_importer/util/read_pxo_file.gd
+++ addons/godot_pixelorama_importer/util/read_pxo_file.gd
@@ -36,24 +36,45 @@ static func read_pxo_file(source_file: String, image_save_path: String):
 	var spritesheet = Image.new()
 	spritesheet.create(size.x * frame_count, size.y, false, Image.FORMAT_RGBA8)
 
-	for i in range(project.frames.size()):
+	var cel_data_size: int = size.x * size.y * 4
+
+	for i in range(frame_count):
 		var frame = project.frames[i]
 
 		# Prepare the frame image
 		var frame_img: Image = null
+		var layer := 0
 		for cel in frame.cels:
-			# Load the cel image
-			var cel_img = Image.new()
-			cel_img.create_from_data(size.x, size.y, false, Image.FORMAT_RGBA8, file.get_buffer(size.x * size.y * 4))
+			var opacity: float = cel.opacity
 
-			if frame_img == null:
-				frame_img = cel_img
+			if project.layers[layer].visible and opacity > 0.0:
+				# Load the cel image
+				var cel_img := Image.new()
+				cel_img.create_from_data(size.x, size.y, false, Image.FORMAT_RGBA8, file.get_buffer(cel_data_size))
+
+				if opacity < 1.0:
+					cel_img.lock()
+					for x in range(size.x):
+						for y in range(size.y):
+							var color := cel_img.get_pixel(x, y)
+							color.a *= opacity
+							cel_img.set_pixel(x, y, color)
+					cel_img.unlock()
+
+				if frame_img == null:
+					frame_img = cel_img
+				else:
+					# Overlay each Cel on top of each other
+					frame_img.blend_rect(cel_img, Rect2(Vector2.ZERO, size), Vector2.ZERO)
 			else:
-				# Overlay each Cel on top of each other
-				frame_img.blend_rect(cel_img, Rect2(Vector2.ZERO, size), Vector2.ZERO)
+				# Skip this cel's data
+				file.seek(file.get_position() + cel_data_size)
 
-		# Add to the spritesheet
-		spritesheet.blit_rect(frame_img, Rect2(Vector2.ZERO, size), Vector2((size.x * i), 0))
+			layer += 1
+
+		if frame_img != null:
+			# Add to the spritesheet
+			spritesheet.blit_rect(frame_img, Rect2(Vector2.ZERO, size), Vector2((size.x * i), 0))
 
 	save_stex(spritesheet, image_save_path)
 	result.value = project
